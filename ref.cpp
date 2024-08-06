f366ad41c48c927668f1cf694bd439c695e5b5a9
MeiRR17 <111447264+MeiRR17@users.noreply.github.com>
2024-08-06 08:53:54 +0300

    Create ref.cpp

---
+++ ref.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void changeByVal(int x);
+void changeByRef(int &x);
+
+int main() 
+{
+    int num = 10;
+    cout << "orig num = " << num << endl;
+    changeByVal(num);
+    cout << "after ByVal: num = " << num <<endl;
+    changeByRef(num);
+    cout << "after ByVal: num = " << num <<endl;
+    
+    return 0;
+}
+void changeByRef(int & x){
+  x = 4;
+}
+void changeByVal(int x){
+  x = 4;
+}
